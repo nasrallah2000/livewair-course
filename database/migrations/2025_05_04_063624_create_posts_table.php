24db3649d4cb07e7e11cda8b883519951a4bd770
Nasrallah <nsrallahh333@gmail.com>
2025-05-04 09:49:24 +0300

    make tables and relations

--- database/migrations/2025_05_04_063624_create_posts_table.php
+++ database/migrations/2025_05_04_063624_create_posts_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
+            $table->foreignId('category_id')->constrained('categories')->cascadeOnDelete();
+            $table->string('title');
+            $table->text('body');
+            $table->string('image');
             $table->timestamps();
         });
     }
